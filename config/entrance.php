7b32841c328f46360845a98d3cd6799abf2c5b4f
BrooksYang <brooksyang@outlook.com>
2018-04-24 23:16:49 +0800

    安装Entrance

---
+++ config/entrance.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    // Cache tag prefix
+    'cache_tag_prefix' => env('CACHE_TAG_PREFIX', 'ex'),
+
+    // Cache ttl
+    'cache_ttl' => env('CACHE_TTL', 120),
+
+    // Admin tables and model.
+    'users_table' => 'auth_admins',
+    'user' => BrooksYang\Entrance\Models\Administrator::class,
+
+    // Role tables and model.
+    'roles_table' => 'auth_roles',
+    'role' => BrooksYang\Entrance\Models\Role::class,
+
+    // Permission tables and model.
+    'permissions_table' => 'auth_permissions',
+    'permission' => BrooksYang\Entrance\Models\Permission::class,
+
+    // Module tables and model.
+    'modules_table' => 'auth_modules',
+    'module' => BrooksYang\Entrance\Models\Module::class,
+
+    // Group tables and model.
+    'groups_table' => 'auth_groups',
+    'group' => BrooksYang\Entrance\Models\Group::class,
+
+    // Pivot table.
+    'permission_role_table' => 'auth_permission_role',
+];
