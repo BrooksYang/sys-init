479a250367fc672340f3cc54c83f8f3dfd5772c6
Zhaiwei <zhaiweiwei@easy2study.net>
2018-05-03 21:53:43 +0800

    引入ckfinder 支持图文混编

---
+++ public/assets/ckeditor/plugins/html5video/lang/zh-cn.js
@@ -0,0 +1,15 @@
+CKEDITOR.plugins.setLang( 'html5video', 'zh-cn', {
+    button: '插入 HTML5 视频',
+    title: 'HTML5 视频',
+    infoLabel: '视频信息',
+    allowed: '允许的文件类型: MP4, WebM, Ogv',
+    urlMissing: '缺少视频源的URL.',
+    videoProperties: '视频属性',
+    upload: '上传',
+    btnUpload: '上传至服务器',
+    advanced: '高级',
+    autoplay: '是否自动播放?',
+    yes: '是',
+    no: '否',
+	responsive: '响应式宽度'
+} );
