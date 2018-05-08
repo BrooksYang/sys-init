a6db093ac7d0ee03ae1f83fbce66a3843a76b0e1
Zhaiwei <zhaiweiwei@easy2study.net>
2018-05-08 20:58:32 +0800

    新增错误页面 404-500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,38 @@
+@extends('entrance::layouts.default')
+
+@section('css-part')
+    @parent
+@endsection
+
+@section('content')
+    <div class="wrap-fluid" id="paper-bg">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="not-found">
+
+                    <img class="img-responsive" alt="" src="{{ url('vendor/entrance/img/500.png') }}">
+
+                    <p>服务器异常...
+
+                    <form class="search-form not-found-search">
+                        <div class="input-group">
+                            <div class="input-group-btn">
+                                <a href="{{ url('demo')}}" class="btn bg-red">返回首页</a>
+                            </div>
+                        </div>
+                        <!-- /.input-group -->
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+
+@section('js-part')
+    <script>
+    </script>
+@endsection
