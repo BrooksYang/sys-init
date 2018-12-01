34a8cc89e0cd0783729cd83737f2f5d1d1784772
Zhaiwei <zhaiweiwei@easy2study.net>
2018-12-01 18:12:22 +0800

    系统及用户记账钱包增加误差金额-展示

--- resources/views/wallet/sysWalletIndex.blade.php
+++ resources/views/wallet/sysWalletIndex.blade.php
@@ -59,7 +59,7 @@
                                     </td>
                                 </tr>
                             @empty
-                                <tr><td colspan="5" class="text-center">
+                                <tr><td colspan="6" class="text-center">
                                         <div class="noDataValue">
                                             暂无数据
                                         </div>
