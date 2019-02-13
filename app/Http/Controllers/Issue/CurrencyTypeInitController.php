6fd9bb8fa811c89bb28c02762b9aad646f6955e1
Zhaiwei <zhaiweiwei@easy2study.net>
2019-02-13 17:51:19 +0800

    修复币种图标上传

--- app/Http/Controllers/Issue/CurrencyTypeInitController.php
+++ app/Http/Controllers/Issue/CurrencyTypeInitController.php
@@ -162,7 +162,8 @@ class CurrencyTypeInitController extends Controller
     public function upload($dir)
     {
 
-        $this->imgUpload(base64_decode($dir));
+        $maxWidth = $maxHeight = 600;
+        $this->imgUpload(base64_decode($dir), $maxWidth, $maxHeight);
     }
 
     /**
