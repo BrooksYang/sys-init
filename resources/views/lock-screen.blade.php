5fd64801fef6414403d9f1beb7ad4fce3eb2fd23
Zhaiwei <zhaiweiwei@easy2study.net>
2019-03-17 16:29:53 +0800

    锁屏页面-禁退

--- resources/views/lock-screen.blade.php
+++ resources/views/lock-screen.blade.php
@@ -160,6 +160,12 @@
         $(window).resize(function() {
             $(".center").center();
         });
+
+        //防止页面后退
+        history.pushState(null, null, document.URL);
+        window.addEventListener('popstate', function () {
+            history.pushState(null, null, document.URL);
+        });
     });
 
     /*  */
