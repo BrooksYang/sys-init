ebb17a07c63eb364466d4d76c30d1ccde5392a35
Zhaiwei <zhaiweiwei@easy2study.net>
2019-03-18 10:21:36 +0800

    提取-页面子视图 -dateTimePicker 样式和页面组件 -hobfont 字体文件 -页面排序和无数据

---
+++ resources/views/component/dateTimeScript.blade.php
@@ -0,0 +1,6 @@
+
+{{-- date-time-picker-js --}}
+<script type='text/javascript' src='{{ asset('vendor/entrance/js/timepicker/bootstrap-timepicker.js') }}'></script>
+<script type='text/javascript' src='{{ asset('vendor/entrance/js/datepicker/bootstrap-datepicker.js') }}'></script>
+<script type='text/javascript' src='{{ asset('vendor/entrance/js/datepicker/clockface.js') }}'></script>
+<script type='text/javascript' src='{{ asset('vendor/entrance/js/datepicker/bootstrap-datetimepicker.js') }}'></script>
