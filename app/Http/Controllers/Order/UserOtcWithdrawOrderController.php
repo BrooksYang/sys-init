9753fc145246827eede09bc4ccf2a1c562d94f05
Zhaiwei <zhaiweiwei@easy2study.net>
2019-04-21 21:27:54 +0800

    修复 - OTC提币订单id 获取 修改日志记录时间

--- app/Http/Controllers/Order/UserOtcWithdrawOrderController.php
+++ app/Http/Controllers/Order/UserOtcWithdrawOrderController.php
@@ -68,7 +68,7 @@ class UserOtcWithdrawOrderController extends Controller
             ->join('dcuex_crypto_currency as currency','withdraw.currency_id','currency.id')  //币种
             ->join('otc_pay_paths as otc_pay','withdraw.pay_path_id','otc_pay.id'); //用户线下收款账户
 
-        $select = [  'withdraw.*', 'u.username', 'u.phone',
+        $select = ['withdraw.id as uid',  'withdraw.*', 'u.username', 'u.phone',
             'currency.currency_title_cn','currency.currency_title_en_abbr',
             'otc_pay.*'];
 
@@ -76,7 +76,7 @@ class UserOtcWithdrawOrderController extends Controller
             $userOtcWithdrawOrderQuery = $userOtcWithdrawOrderQuery
                 ->join('dcuex_user_crypto_wallet as u_wallet','withdraw.wallet_id','u_wallet.id'); //用户真实钱包
 
-            $select = [  'withdraw.*', 'u.username', 'u.phone',
+            $select = ['withdraw.id as uid', 'withdraw.*', 'u.username', 'u.phone',
                 'currency.currency_title_cn','currency.currency_title_en_abbr',
                 'otc_pay.*',
                 'u_wallet.crypto_wallet_title','u_wallet.crypto_wallet_address'];
