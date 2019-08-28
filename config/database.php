6aa3b4df12195355e45950e00bc86fd5666e69c6
Zhaiwei <zhaiweiwei@easy2study.net>
2019-08-28 22:17:14 +0800

    修改 - 恢复config配置

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '103.240.181.123'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bitgoodex'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'btbreil2018!)'),
+            'database' => env('DB_DATABASE', 'dcuex'),
+            'username' => env('DB_USERNAME', 'dcu'),
+            'password' => env('DB_PASSWORD', 'dcu@2018!)'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
