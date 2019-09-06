d6311f57173765c25352d2285939873067892723
Zhaiwei <zhaiweiwei@easy2study.net>
2019-09-07 00:05:35 +0800

    修改 - 页面favicon

--- resources/views/vendor/entrance/layouts/default.blade.php
+++ resources/views/vendor/entrance/layouts/default.blade.php
@@ -114,7 +114,7 @@
                 <a href="javascript:;">
                     {{ config('app.copyright', 'Laravel') }}
                 </a>
-                <img src="{{ asset('logo_80.png') }}" alt="" style="width: 25px">
+                {{--<img src="{{ asset('logo_80.png') }}" alt="" style="width: 25px">--}}
                {{-- Made with
                 <i class="fontello-heart-filled text-red"></i>--}}
             </div>
