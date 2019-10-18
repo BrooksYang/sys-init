81097fc7478d153f8083f528d59e44b9a86b0661
Zhaiwei <zhaiweiwei@easy2study.net>
2019-10-18 10:53:14 +0800

    调试 - 管理员退出

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,7 +56,7 @@ class LoginController extends Controller
 
         $this->guard()->logout();
 
-        $request->session()->invalidate();
+        $request->session()->flush();
 
         return redirect('/');
     }
